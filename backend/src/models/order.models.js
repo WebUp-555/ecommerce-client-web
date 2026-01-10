1da831f1e8a17626f6fc1b5d5139d5f30097df32
Shamsheer <designcore.dev@gmail.com>
2026-01-10 19:45:24 +0530

    feat: Add Razorpay integration and Nodemailer for order processing

--- backend/src/models/order.models.js
+++ backend/src/models/order.models.js
@@ -1,44 +1,61 @@
+import mongoose from "mongoose";
 
-import mongoose, {Schema} from "mongoose";
-const orderSchema=new mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,ref:'User',
-        required:true
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
     },
-     items:[{
-        productId:{
-            type:mongoose.Schema.Types.ObjectId,ref:'Product',
-            required:true
+
+    items: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true
         },
-        name:{
-            type:String,
-            required:true},
-        price:{
-            type:Number,
-            required:true
-        },
-        quantity:{
-            type:Number,
-            required:true,
-            default:1
-        }
-    }],
-    address:{
-        fullAddress:{type:String,required:true},
-        city:{type:String,required:true},
-        state:{type:String,required:true},  
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true, default: 1 }
+      }
+    ],
+
+    address: {
+      fullAddress: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true }
     },
-    pricing:{
-        itemsTotal:{type:Number,required:true},
-        tax:{type:Number,required:true},
-        shippingCharges:{type:Number,required:true},
-        discount:{type:Number,required:true},
-        totalAmount:{type:Number,required:true},
+
+    pricing: {
+      itemsTotal: { type: Number, required: true },
+      tax: { type: Number, required: true },
+      shippingCharges: { type: Number, required: true },
+      discount: { type: Number, required: true },
+      totalAmount: { type: Number, required: true }
     },
-    status:{
-        type:String,
-        enum:["Created","PAID","Processing","Shipped","Delivered","Cancelled","Failed"],
-        default:"Created"
+
+    razorpay: {
+      orderId: String,
+      paymentId: String,
+      signature: String
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "CREATED",
+        "PAID",
+        "PROCESSING",
+        "SHIPPED",
+        "DELIVERED",
+        "CANCELLED",
+        "FAILED"
+      ],
+      default: "CREATED"
     }
-},{timestamps:true});
-export const Order=mongoose.model("Order",orderSchema);
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
